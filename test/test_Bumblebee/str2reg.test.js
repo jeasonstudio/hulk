3d14278f42aed5651fb5239ba8159b5c57150b71
Jeason <zhaojitong@xiaomi.com>
2017-07-31 13:43:01 +0800

    <test> add test

--- test/test_Bumblebee/str2reg.test.js
+++ test/test_Bumblebee/str2reg.test.js
@@ -6,49 +6,49 @@ const expect = require('chai').expect;
 describe('STRING to REGEXP', () => {
   it('should match simple url 1 post', () => {
     const result = str2reg('/pet', 'post');
-    expect(result).to.deep.equal(/\/pet$/);
-    expect(result.test('/pet')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet$/);
+    expect(eval(result).test('/pet')).to.be.equal(true);
   });
 
   it('should match simple url 2 get', () => {
     const result = str2reg('/pet/findByTags?a=2', 'get');
-    expect(result).to.deep.equal(/\/pet\/findByTags\?/);
-    expect(result.test('/pet/findByTags?a=2')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/findByTags\?/);
+    expect(eval(result).test('/pet/findByTags?a=2')).to.be.equal(true);
   });
 
   it('should match simple url 2 post', () => {
     const result = str2reg('/pet/findByTags', 'post');
-    expect(result).to.deep.equal(/\/pet\/findByTags$/);
-    expect(result.test('/pet/findByTags')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/findByTags$/);
+    expect(eval(result).test('/pet/findByTags')).to.be.equal(true);
   });
 
   it('should match {param} in url post', () => {
     const result = str2reg('/pet/{userid}/uploadimg', 'post');
-    expect(result).to.deep.equal(/\/pet\/\S+\/uploadimg$/);
-    expect(result.test('/pet/aaa/uploadimg')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/\S+\/uploadimg$/);
+    expect(eval(result).test('/pet/aaa/uploadimg')).to.be.equal(true);
   });
 
   it('should match {param} in url get', () => {
     const result = str2reg('/pet/{userid}/uploadimg', 'get');
-    expect(result).to.deep.equal(/\/pet\/\S+\/uploadimg\?/);
-    expect(result.test('/pet/aaa/uploadimg?aa=22')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/\S+\/uploadimg\?/);
+    expect(eval(result).test('/pet/aaa/uploadimg?aa=22')).to.be.equal(true);
   });
 
   it('should match {param} after url post', () => {
     const result = str2reg('/pet/uploadimg/{userid}', 'post');
-    expect(result).to.deep.equal(/\/pet\/uploadimg\/\S+$/);
-    expect(result.test('/pet/uploadimg/aaa/')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/uploadimg\/\S+$/);
+    expect(eval(result).test('/pet/uploadimg/aaa/')).to.be.equal(true);
   });
 
   it('should match {param} after url get', () => {
     const result = str2reg('/pet/uploadimg/{userid}', 'get');
-    expect(result).to.deep.equal(/\/pet\/uploadimg\/\S+\?/);
-    expect(result.test('/pet/uploadimg/aaa?s=2/')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/uploadimg\/\S+\?/);
+    expect(eval(result).test('/pet/uploadimg/aaa?s=2/')).to.be.equal(true);
   });
 
   it('should match method put', () => {
     const result = str2reg('/pet/uploadimg/{userid}', 'put');
-    expect(result).to.deep.equal(/\/pet\/uploadimg\/\S+$/);
-    expect(result.test('/pet/uploadimg/aaa/')).to.be.equal(true);
+    expect(eval(result)).to.deep.equal(/\/pet\/uploadimg\/\S+$/);
+    expect(eval(result).test('/pet/uploadimg/aaa/')).to.be.equal(true);
   });
 });
